8328b0504545797c78a10078d6ce7645e047f953
Max Tyler <maxastyler@gmail.com>
2023-09-11 21:51:30 +0100

    Update expressions and stuff

--- src/expression.rs
+++ src/expression.rs
@@ -1,78 +1,48 @@
+use crate::tokeniser::Location;
+
 #[derive(PartialEq, Clone, Debug, Hash, Eq)]
 pub struct Symbol(pub String);
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Literal {
-    Int(i64),
-    Double(f64),
+pub enum Literal<'a> {
+    Number(f64),
     Nil,
     Bool(bool),
+    String(String),
+    Quoted(Symbol),
+    List(Vec<LocatedExpression<'a>>),
+    Dictionary(Vec<(LocatedExpression<'a>, LocatedExpression<'a>)>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Let<'a> {
+    recursive: bool,
+    pairs: Vec<(Symbol, LocatedExpression<'a>)>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Block<'a> {
+    scope_introducing: bool,
+    ignored: Vec<LocatedExpression<'a>>,
+    last: Box<LocatedExpression<'a>>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Expression {
-    Condition(Vec<(Expression, Expression)>, Box<Expression>),
-    Call(Box<Expression>, Vec<Expression>),
-    Assign(Symbol, Box<Expression>),
-    Function(Vec<Symbol>, Box<Expression>),
-    Block(Vec<Expression>, Box<Expression>),
-    Literal(Literal),
+pub struct LocatedExpression<'a> {
+    expression: Expression<'a>,
+    location: Location<'a>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expression<'a> {
+    Condition(
+        Vec<(LocatedExpression<'a>, LocatedExpression<'a>)>,
+        Box<LocatedExpression<'a>>,
+    ),
+    Call(Box<LocatedExpression<'a>>, Vec<LocatedExpression<'a>>),
+    Let(Let<'a>),
+    Function(Vec<Symbol>, Box<LocatedExpression<'a>>),
+    Block(Block<'a>),
+    Literal(Literal<'a>),
     Symbol(Symbol),
-}
-
-impl From<Symbol> for Expression {
-    fn from(value: Symbol) -> Self {
-        Expression::Symbol(value)
-    }
-}
-
-impl From<i64> for Expression {
-    fn from(value: i64) -> Self {
-        Expression::Literal(Literal::Int(value))
-    }
-}
-
-impl From<bool> for Expression {
-    fn from(value: bool) -> Self {
-        Expression::Literal(Literal::Bool(value))
-    }
-}
-
-impl From<(Expression, Vec<Expression>)> for Expression {
-    fn from(value: (Expression, Vec<Expression>)) -> Self {
-        Expression::Call(value.0.into(), value.1)
-    }
-}
-
-impl From<(&str, Expression)> for Expression {
-    fn from(value: (&str, Expression)) -> Self {
-        Expression::Assign(Symbol(value.0.into()), value.1.into())
-    }
-}
-
-impl From<(Vec<&str>, Expression)> for Expression {
-    fn from(value: (Vec<&str>, Expression)) -> Self {
-        Expression::Function(
-            value.0.into_iter().map(|x| Symbol(x.into())).collect(),
-            value.1.into(),
-        )
-    }
-}
-
-impl From<Vec<Expression>> for Expression {
-    fn from(value: Vec<Expression>) -> Self {
-        match &value[..] {
-            [ref ignored @ .., last] => Expression::Block(
-                ignored.into_iter().map(|x| (*x).clone()).collect(),
-                last.clone().into(),
-            ),
-            _ => panic!("Block expression vector should have 1 or more elements"),
-        }
-    }
-}
-
-impl From<&str> for Expression {
-    fn from(value: &str) -> Self {
-        Expression::Symbol(Symbol(value.into()))
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,42 @@
-use std::rc::Rc;
+// use std::rc::Rc;
 
-use compiler::Compiler;
-use parser::parse_program;
-use vm::VM;
+// use compiler::Compiler;
+// use parser::parse_program;
+// use vm::VM;
 
-use crate::{
-    expression::{Expression, Symbol},
-    value::{Closure, Function},
-};
+// use crate::{
+//     expression::{Expression, Symbol},
+//     value::{Closure, Function},
+// };
 
-mod compiler;
+// mod compiler;
 mod expression;
-mod frame;
-mod native_function;
-mod opcode;
+// mod frame;
+// mod native_function;
+// mod opcode;
 mod parser;
 mod tokeniser;
-mod value;
-mod vm;
+// mod value;
+// mod vm;
 
 fn main() {
-    let (s, e) = parse_program(include_str!("./fac.maxlang")).unwrap();
-    let mut c = Compiler::new();
-    c.compile_expression(None, &e[0], true).unwrap();
-    let f = c.frame_to_function();
+    // let (s, e) = parse_program(include_str!("./fac.maxlang")).unwrap();
+    // let mut c = Compiler::new();
+    // c.compile_expression(None, &e[0], true).unwrap();
+    // let f = c.frame_to_function();
 
-    let mut vm = VM::from_bare_function(f);
-    loop {
-        match vm.step() {
-            Ok(Some(v)) => {
-                println!("GOT A VALUE: {:?}", v);
-                break;
-            }
-            Err(s) => {
-                println!("{:?}", s);
-                break;
-            }
-            _ => (),
-        }
-    }
+    // let mut vm = VM::from_bare_function(f);
+    // loop {
+    //     match vm.step() {
+    //         Ok(Some(v)) => {
+    //             println!("GOT A VALUE: {:?}", v);
+    //             break;
+    //         }
+    //         Err(s) => {
+    //             println!("{:?}", s);
+    //             break;
+    //         }
+    //         _ => (),
+    //     }
+    // }
 }
