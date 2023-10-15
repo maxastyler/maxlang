ddaf73c75d006c8fefd50bce412e9e120e721383
Max Tyler <maxastyler@gmail.com>
2023-10-15 22:37:10 +0100

    Add first implementation of the heap

--- src/heap.rs
+++ src/heap.rs
@@ -1,8 +1,14 @@
-use std::hash::{BuildHasher, Hasher};
+use std::{
+    alloc::{alloc, Layout},
+    fmt::Debug,
+    hash::{BuildHasher, Hasher},
+    mem::{align_of, size_of},
+    ptr::null_mut,
+};
 
-use im::HashMap;
+use im::{HashMap, Vector};
 
-use crate::value::Value;
+use std::alloc::dealloc;
 
 struct FNVHasher(u32);
 
@@ -17,6 +23,12 @@ impl Hasher for FNVHasher {
             self.0 *= 16777619;
         }
     }
+}
+
+#[derive(Clone)]
+enum Value {
+    Number(f64),
+    Object(*mut HeapElement),
 }
 
 struct FNVBuilder;
@@ -35,6 +47,31 @@ fn new_hash_map() -> HashMap<Value, Value, FNVBuilder> {
 
 pub enum Object {
     HashMap(HashMap<Value, Value, FNVBuilder>),
+    List(Vector<Value>),
+}
+
+impl Object {
+    fn add_children_to_grey_list(&self, grey_list: &mut Vec<*mut HeapElement>) {
+        match self {
+            Object::HashMap(h) => {
+                for (k, v) in h.iter() {
+                    if let Value::Object(o) = k {
+                        grey_list.push(*o);
+                    }
+                    if let Value::Object(o) = v {
+                        grey_list.push(*o);
+                    }
+                }
+            }
+            Object::List(l) => {
+                for v in l.iter() {
+                    if let Value::Object(o) = v {
+                        grey_list.push(*o);
+                    }
+                }
+            }
+        }
+    }
 }
 
 pub struct HeapElement {
@@ -44,5 +81,101 @@ pub struct HeapElement {
 }
 
 pub struct Heap {
-    start: Option<*mut Object>,
+    start: *mut HeapElement,
+}
+
+impl Debug for Heap {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("hi");
+        Ok(())
+    }
+}
+
+impl Heap {
+    fn new() -> Self {
+        Self { start: null_mut() }
+    }
+
+    fn trace(&self, roots: Vec<*mut HeapElement>) {
+        let mut grey_list = roots;
+        while let Some(ptr) = grey_list.pop() {
+            let heap_elem = unsafe { ptr.as_mut().unwrap() };
+            heap_elem.marked = true;
+            (*heap_elem)
+                .object
+                .add_children_to_grey_list(&mut grey_list);
+        }
+    }
+
+    fn sweep(&mut self) {
+        let mut prev = null_mut();
+        let mut current = self.start;
+        while !current.is_null() {
+            if unsafe { (*current).marked } {
+                unsafe { (*current).marked = false };
+                prev = current;
+                current = unsafe { (*current).next };
+            } else {
+                let unreached = current;
+                current = unsafe { (*current).next };
+                if prev.is_null() {
+                    self.start = current;
+                } else {
+                    unsafe {
+                        (*prev).next = current;
+                    }
+                }
+                unsafe {
+                    unreached.drop_in_place();
+                    dealloc(
+                        unreached.cast(),
+                        Layout::from_size_align_unchecked(
+                            size_of::<HeapElement>(),
+                            align_of::<HeapElement>(),
+                        ),
+                    )
+                }
+            }
+        }
+    }
+
+    fn alloc(&mut self, object: Object) -> Value {
+        let new_ptr = unsafe {
+            alloc(Layout::from_size_align_unchecked(
+                size_of::<HeapElement>(),
+                align_of::<HeapElement>(),
+            ))
+        }
+        .cast();
+        unsafe {
+            (*new_ptr) = HeapElement {
+                object,
+                marked: false,
+                next: self.start,
+            };
+            self.start = new_ptr;
+        };
+        Value::Object(new_ptr)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use im::Vector;
+
+    use super::Heap;
+
+    #[test]
+    fn heap_works() {
+        let mut heap = Heap::new();
+        let o = heap.alloc(super::Object::List(Vector::from_iter(
+            [2, 3, 4].iter().map(|x| super::Value::Number(*x as f64)),
+        )));
+        heap.trace(vec![match o {
+            crate::heap::Value::Object(o) => o,
+            _ => unreachable!(),
+        }]);
+        heap.sweep();
+        assert!(heap.start.is_null());
+    }
 }
